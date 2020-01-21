befcd584a58a384ef05b0fd1b1b14e9452b94394
Despanza <despanza@protonmail.com>
2020-01-21 18:36:25 -0500

    1st draft of project 1

---
+++ src/Project1.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Project1 {
+    public static void main(String[] args) {
+        int[] weight;
+        JFileChooser Chooser = new JFileChooser();
+
+    }
+}
---
+++ src/Weight.java
@@ -0,0 +1,23 @@
+public class Weight {
+    public int pounds;
+    public int ounces;
+    public Weight(int pounds, int ounces){
+        this.pounds = pounds;
+        this.ounces = ounces;
+    }
+    public Weight(){
+        this.pounds = 20;
+        this.ounces = 20;
+    }
+    public int lessThan(int weight){
+        
+        return weight;
+
+    }
+    public int addTO(int weight){
+        return weight;
+    }
+    public double divide(int weightD){
+        return weightD;
+    }
+}
